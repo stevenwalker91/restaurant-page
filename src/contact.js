51fd90b467336b9a7e38ebf76ffe326418245cc8
woftis <stevenwalker1991@googlemail.com>
2022-11-09 14:47:44 +0000

    added basic phone number to page - didn't think this project merited much more as the key objective has been met

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,7 @@ export function generateContactPage () {
     //check its connecting
     console.log('hello');
 
-    const testhtml = '<div class="container-black"><div class="container"><h1>Contact</h1></div></div>'
+    const testhtml = '<div class="container-black"><div class="container"><div class="inner-container"><h1>Contact</h1><p>Phone: 0777777777</p></div></div></div>'
 
     return testhtml;
 }
